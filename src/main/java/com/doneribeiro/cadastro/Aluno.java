85038cd7ba92aed2e3e0d4b3487c178b25970a7e
Done Ribeiro <www.done_@hotmail.com>
2019-03-26 20:46:16 -0300

    Primeiro Projeto Angular

---
+++ src/main/java/com/doneribeiro/cadastro/Aluno.java
@@ -0,0 +1,39 @@
+package com.doneribeiro.cadastro;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Aluno {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	private String curso;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+}
